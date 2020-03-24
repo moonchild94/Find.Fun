58fd2a4443c8b212321e0bd3e23d042ec013130c
dagny <moonchild174@gmail.com>
2020-03-24 12:17:18 +0200

    Initial Commit

---
+++ Find.Fun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Find.Fun
+//
+//  Created by Daria Kalmykova on 24.03.2020.
+//  Copyright © 2020 Daria Kalmykova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
